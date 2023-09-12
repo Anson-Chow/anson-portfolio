d33c9f291b167d07829263466b90fb5a3e85c015
Anson <chow.anson25@gmail.com>
2023-09-12 13:48:14 -0400

    Cleaning up

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -90,8 +90,8 @@ const WorkSlider = () => {
                       {/* overlay gradient */}
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       {/* title */}
-                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex flex-col items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-2 group-hover:xl:-translate-y-20 group-hover:sm:-translate-y-12 transition-all duration-300">
+                        <div className="flex flex-col items-center gap-x-2 text-[8px] sm:text-[13px] md:text-[14px] lg:text-[14px] tracking-[0.2em]">
                           {/* title part 1 */}
                           <div className="delay-100">{image.title}</div>
                           {/* icon and technology paragraph */}
